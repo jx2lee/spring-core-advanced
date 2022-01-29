846812fce05aed80c4d54cc83ba51a086c08dbd4
jx2lee <jaejun.lee.1991@gmail.com>
2022-01-29 22:00:23 +0900

    스프링 핵심 원리 고급편: 스프링이 지원하는 프록시: 예제 코드2: 직접 만든 포인트컷: Pointcut && MethodMatcher

--- proxy/src/test/java/io/github/jx2lee/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/io/github/jx2lee/proxy/advisor/AdvisorTest.java
@@ -3,11 +3,17 @@ package io.github.jx2lee.proxy.advisor;
 import io.github.jx2lee.proxy.common.ServiceImpl;
 import io.github.jx2lee.proxy.common.ServiceInterface;
 import io.github.jx2lee.proxy.common.advice.TimeAdvice;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 
+import java.lang.reflect.Method;
+
+@Slf4j
 public class AdvisorTest {
 
     @Test
@@ -20,5 +26,51 @@ public class AdvisorTest {
 
         proxy.call();
         proxy.find();
+    }
+
+    @Test
+    void advisorTest2() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.call();
+        proxy.find();
+    }
+
+    static class MyPointcut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+        private String matchName = "find";
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("method={}. targetClass={}", method.getName(), targetClass);
+            log.info("result={}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
