dcf6b85de3d9ed567544f4e7fda83ec5f787ccdd
jx2lee <jaejun.lee.1991@gmail.com>
2022-02-01 16:20:44 +0900

    스프링 핵심 원리 고급편: 빈 후처리기: 스프링이 제공하는 빈 후처리기: 1: Advisor 를 단순히 bean 등록

--- proxy/src/main/java/io/github/jx2lee/proxy/ProxyApplication.java
+++ proxy/src/main/java/io/github/jx2lee/proxy/ProxyApplication.java
@@ -9,6 +9,7 @@ import io.github.jx2lee.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import io.github.jx2lee.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import io.github.jx2lee.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import io.github.jx2lee.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import io.github.jx2lee.proxy.config.v5_autoproxy.AutoProxyConfig;
 import io.github.jx2lee.proxy.trace.logtrace.LogTrace;
 import io.github.jx2lee.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -23,7 +24,8 @@ import org.springframework.context.annotation.Import;
 // @Import(DynamicProxyFilterConfig.class)
 // @Import(ProxyFactoryConfigV1.class)
 // @Import(ProxyFactoryConfigV2.class)
-@Import(BeanPostProcessorConfig.class)
+// @Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "io.github.jx2lee.proxy.app") //주의, config 패키지는 자동 component 스캔 대상이 되지 않도록 하기 위해 Base package 설정
 public class ProxyApplication {
 
---
+++ proxy/src/main/java/io/github/jx2lee/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,26 @@
+package io.github.jx2lee.proxy.config.v5_autoproxy;
+
+import io.github.jx2lee.proxy.config.AppV1Config;
+import io.github.jx2lee.proxy.config.AppV2Config;
+import io.github.jx2lee.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import io.github.jx2lee.proxy.trace.logtrace.LogTrace;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+    @Bean
+    public Advisor advisor1(LogTrace trace) {
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("save*", "request*", "order*");
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+}
