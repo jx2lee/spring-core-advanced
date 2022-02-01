fca57168ace8b02a9499ba008f58ce0619d4430a
jx2lee <jaejun.lee.1991@gmail.com>
2022-02-01 16:31:03 +0900

    스프링 핵심 원리 고급편: 빈 후처리기: 스프링이 제공하는 빈 후처리기: 2: AssertJExpression 문법을 통한 상세한 Pointcut

--- proxy/src/main/java/io/github/jx2lee/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ proxy/src/main/java/io/github/jx2lee/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import io.github.jx2lee.proxy.config.AppV2Config;
 import io.github.jx2lee.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import io.github.jx2lee.proxy.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +16,7 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
+    // @Bean
     public Advisor advisor1(LogTrace trace) {
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("save*", "request*", "order*");
@@ -23,4 +24,19 @@ public class AutoProxyConfig {
         return new DefaultPointcutAdvisor(pointcut, advice);
     }
 
+    // @Bean
+    public Advisor advisor2(LogTrace trace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* io.github.jx2lee.proxy.app..*(..))");
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace trace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* io.github.jx2lee.proxy.app..*(..)) && !execution(* io.github.jx2lee.proxy.app..noLog(..))");
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
 }
