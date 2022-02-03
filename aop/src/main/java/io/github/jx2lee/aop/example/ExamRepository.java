76601617c709df4b30819e2f86a1b4432fbb4e74
jx2lee <jaejun.lee.1991@gmail.com>
2022-02-03 19:18:27 +0900

    스프링 핵심 원리 고급편: spring AOP 예제: 로그 출력 AOP

--- aop/src/main/java/io/github/jx2lee/aop/example/ExamRepository.java
+++ aop/src/main/java/io/github/jx2lee/aop/example/ExamRepository.java
@@ -1,11 +1,13 @@
 package io.github.jx2lee.aop.example;
 
+import io.github.jx2lee.aop.example.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ExamRepository {
     private static int seq = 0;
 
+    @Trace
     public String save(String itemId) {
         seq++;
         if (seq % 5 == 0) {
---
+++ aop/src/main/java/io/github/jx2lee/aop/example/aop/TraceAspect.java
@@ -0,0 +1,19 @@
+package io.github.jx2lee.aop.example.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Aspect
+public class TraceAspect {
+
+    @Before("@annotation(io.github.jx2lee.aop.example.annotation.Trace)")
+    public void doTrace(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        log.info("[trace] {} args={}", joinPoint.getSignature(), args);
+    }
+}
